882bb8e55fb774f9745774c01091c294fe06f125
Aravind.Gogindara <Aravind.Gogindara@GLBM7G3>
2022-06-29 17:53:12 +0100

    test

---
+++ depency_injection_poc/depency_injection_poc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace depency_injection_poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
